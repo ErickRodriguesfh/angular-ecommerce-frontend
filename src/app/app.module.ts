430a2a4312becab489ec1977c5aebde2dceb626d
Erick Bacelar Rodrigues <erickrodrigues0150@gmail.com>
2023-02-27 22:11:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes: Routes = [
+  { path: 'search/:keyword', component: ProductsListComponent },
+  { path: 'category/:id', component: ProductsListComponent },
+  { path: 'category', component: ProductsListComponent },
+  { path: 'products', component: ProductsListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+  providers: [ProductService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProductsList(theCategoryId: number): Observable<Product[]> {
+    //build URL base on the categoryId
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    //build URL base on the keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
+  }
+}
+
+interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  };
+}
