86f9377e15594bc4e36ae5646d281de2084fcb35
Erick Bacelar Rodrigues <erickrodrigues0150@gmail.com>
2023-04-10 20:22:11 -0300

    Implementação de autenticação e login com okta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,24 @@ import { CarStatusComponent } from './components/car-status/car-status.component
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import {
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OKTA_CONFIG,
+} from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import myAppConfig from './config/my-app-config';
+
+const oktaConfig = myAppConfig.oidc;
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
+  { path: 'login/callback', component: OktaCallbackComponent },
+  { path: 'login', component: LoginComponent },
+
   { path: 'checkout', component: CheckoutComponent },
   { path: 'cart-details', component: CartDetailsComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
@@ -37,15 +53,18 @@ const routes: Routes = [
     CarStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent,
   ],
   imports: [
+    OktaAuthModule,
     ReactiveFormsModule,
     BrowserModule,
     HttpClientModule,
     NgbModule,
     RouterModule.forRoot(routes),
   ],
-  providers: [ProductService],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth } }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
